e51b00d5b9e71759062d2e79de6e61e690936a5e
Vanhe <r0808423@student.thomasmore.be>
2021-09-22 14:43:04 +0200

    .idea toegevoegd

--- src/App.js
+++ src/App.js
@@ -3,22 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="root">
+          <div>
+            <h1>Menu</h1>
+            <div>cola</div>
+            <div>water</div>
+            <div>bier</div>
+            <div>wijn</div>
+          </div>
+        </div>
       </header>
-    </div>
+
   );
 }
 
